a70f82e4905c5184d80ee0442f38563e1db14b97
Fred Lundén <fred@Freds-MacBook-Pro.local>
2015-05-21 18:00:33 +0200

    selection interface

--- public/javascripts/deployer.js
+++ public/javascripts/deployer.js
@@ -1,5 +1,43 @@
 var socket = io();
 
+
+$(function() {
+	$('ul li:first-child').addClass('active');
+	
+//	$(document).keydown(function(e) {
+//	    switch(e.which) {
+//	        case 37: 
+////				socket.emit('inputs_left');
+////				unselect();
+//				$('li.active').toggleClass('selected');
+//	        break;
+//	
+//	        case 38: 
+////				socket.emit('inputs_up_test');
+//	        	up();
+//			break;
+//	
+//	        case 39: 
+////				socket.emit('inputs_right');
+////				select();
+//				$('li.active').toggleClass('selected');
+//	        break;
+//	
+//	        case 40:
+////				socket.emit('inputs_down_test');
+//				down();
+//	        break;
+//	
+//	        default: 
+//			return;
+//	    }
+//	    e.preventDefault(); 
+//	});
+	
+});
+
+
+
 socket.on('status_set', function(data) {	
 	var list = $('ul li');
 	var d = JSON.parse(data);
@@ -15,20 +53,47 @@ socket.on('status_set', function(data) {
 	}
 });
 
-socket.on('inputs_up', function() {
+socket.on('inputs_up', function() {	
 	console.log('joystick_up');
+	
+	var activeItem = $('li.active');
+	var activeIndex = $('ul li').index(activeItem);
+	
+	activeItem.removeClass('active');
+	
+	if (activeIndex === 0) {
+		$('ul li:last-child').addClass('active');
+	} else {
+		activeItem.closest('li').addClass('active');
+	}
 });
 
-socket.on('inputs_down', function() {
+socket.on('inputs_down', function() {	
 	console.log('joystick_down');
+	
+	var totalCount = $('ul li').length;
+	var activeItem = $('li.active');
+	var activeIndex = $('ul li').index(activeItem);
+	
+	activeItem.removeClass('active');
+	
+	if (activeIndex === totalCount - 1) {
+		$('ul li:first-child').addClass('active');
+	} else {
+		activeItem.next('li').addClass('active');
+	}
 });
 
 socket.on('inputs_left', function() {
 	console.log('joystick_left');
+
+	$('li.active').removeClass('selected');
 });
 
 socket.on('inputs_right', function() {
 	console.log('joystick_right');
+
+	$('li.active').addClass('selected');
 });
 
 socket.on('inputs_button', function() {
