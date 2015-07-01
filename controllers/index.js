429dbf3cf1aac75628226c41f744b7480cf49e26
Fred Lundén <fred@Freds-MacBook-Pro.local>
2015-07-01 14:46:26 +0200

    dsfdsfsdf

--- controllers/index.js
+++ controllers/index.js
@@ -7,16 +7,16 @@ router.use('/status', require('./status'));
 
 
 router.get('/', function(req, res, next) {
-  
-  // TODO: Make a request to the .net service to get a list of all projects and statuses. Maybe 
-  
+
+  // TODO: Make a request to the .net service to get a list of all projects and statuses. Maybe
+
 //  var projects = [
 //    { id: 0, name: 'Project 1', status: Math.round(Math.random()) },
 //    { id: 1, name: 'Project 2', status: Math.round(Math.random()) },
 //    { id: 2, name: 'Project 3', status: Math.round(Math.random()) },
 //    { id: 3, name: 'Project 4', status: Math.round(Math.random()) }
 //  ];
-    
+
   res.render('index', { title: 'SoftResource Deployer'});
 });
 
