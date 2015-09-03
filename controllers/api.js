fe81cc3bbc0f735dfaeaf614a565d81eee192ecf
Fred Lundén <fred@Freds-MacBook-Pro.local>
2015-09-03 16:06:02 +0200

    asd

---
+++ controllers/api.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+
+module.exports = function(com) {
+  router.get('/', function(req, res, next) {
+    res.send('api');
+  });
+
+  router.post('/setBuilds', function(req, res, next) {
+    com.sockets.builds.set(req.body, function(isSuccess) {
+      res.send({ success: isSuccess });
+    });
+  });
+
+  router.post('/setLatestBuild', function(req, res, next) {
+    com.sockets.builds.setLatest(req.body, function(isSuccess) {
+      res.send({ success: isSuccess });
+    });
+  });
+
+  router.post('/setLatestFailedBuild', function(req, res, next) {
+    com.sockets.builds.setLatestFailed(req.body, function(isSuccess) {
+      res.send({ success: isSuccess });
+    });
+  });
+
+  return router;
+}
